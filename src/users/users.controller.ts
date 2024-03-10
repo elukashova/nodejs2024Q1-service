6122ce2e1f991f1248f81f3f39e885fd6cccec2d
elukashova <ele.lukashova@gmail.com>
2024-03-10 11:49:53 +0100

    feat: add ok status codes to users responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Put,
   UseInterceptors,
   ClassSerializerInterceptor,
+  HttpCode,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/user-create.dto';
@@ -20,24 +21,33 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @HttpCode(200)
   getAllUsers() {
     return this.usersService
       .getAllUsers()
       .map((user) => new UserResponse(user));
   }
+
   @Get(':id')
+  @HttpCode(200)
   getUser(@Param('id') id: string) {
     return new UserResponse(this.usersService.getUser(id));
   }
+
   @Post()
+  @HttpCode(201)
   createUser(@Body() body: CreateUserDto) {
     return new UserResponse(this.usersService.createUser(body));
   }
+
   @Put(':id')
+  @HttpCode(200)
   updateUser(@Param('id') id: string, @Body() body: UpdatePasswordDto) {
     return new UserResponse(this.usersService.updateUser(id, body));
   }
+
   @Delete(':id')
+  @HttpCode(204)
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
   }
